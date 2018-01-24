e6a8e1b958d6f5b649fdd8cbdd2166d6ef17616d
Richard <rwhittaker86@outlook.com>
2018-01-24 01:43:55 -0500

    More variable examples in chapter 3 scratchpad

--- Chapter 3/scratchpad.py
+++ Chapter 3/scratchpad.py
@@ -1,5 +1,4 @@
-first_string = "This is a string"
-second_string = "This is another string"
-first_number  = 4
-second_number = 5
-print("The first variables are %s , %s, %d, %d" % (first_string, second_string, first_number, second_number))
+first_string  = 345
+second_number = "This isn't a number"
+print(first_string)
+print(second_number)
