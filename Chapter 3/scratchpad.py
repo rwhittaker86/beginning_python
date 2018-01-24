3d23b2177e1a1059808e33c0a815de0bc64ab776
Richard <rwhittaker86@outlook.com>
2018-01-24 01:41:18 -0500

    First variables example in chapter 3 scratch pad

---
+++ Chapter 3/scratchpad.py
@@ -0,0 +1,5 @@
+first_string = "This is a string"
+second_string = "This is another string"
+first_number  = 4
+second_number = 5
+print("The first variables are %s , %s, %d, %d" % (first_string, second_string, first_number, second_number))
