6e882380051e9de6b81df66fb5ffacb3eba0a65a
Richard <rwhittaker86@outlook.com>
2018-01-24 01:51:05 -0500

    example of altering named variables in chapter 3 scratchpad

--- Chapter 3/scratchpad.py
+++ Chapter 3/scratchpad.py
@@ -1,2 +1,7 @@
-lightbulbs_in_closet = 10
-lightbulbs_in_lamps = 12
+proverb = "A penny saved"
+proverb = proverb + " is a penny earned"
+print(proverb)
+pennies_saved = 0
+pennies_saved = pennies_saved + 1
+print(pennies_saved)
+print(pennies_saved + 1)
