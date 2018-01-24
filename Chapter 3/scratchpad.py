e79d54b39122d0283d7cfa1eb86b9aa05a42a9ad
Richard <rwhittaker86@outlook.com>
2018-01-24 01:54:59 -0500

    Copying Data example in chapter 3 scratchpad

--- Chapter 3/scratchpad.py
+++ Chapter 3/scratchpad.py
@@ -1,7 +1,6 @@
-proverb = "A penny saved"
-proverb = proverb + " is a penny earned"
-print(proverb)
-pennies_saved = 0
+pennies_saved = 1
+pennies_earned = pennies_saved
+print(pennies_earned)
 pennies_saved = pennies_saved + 1
 print(pennies_saved)
-print(pennies_saved + 1)
+print(pennies_earned)
