5ff06863875769c30625bc5f2499a1909b772a2f
Richard <rwhittaker86@outlook.com>
2018-02-27 12:00:12 -0500

    variable examples in scratchpad

--- Chapter 3/scratchpad.py
+++ Chapter 3/scratchpad.py
@@ -1,4 +1,4 @@
-and, as ,assert, break, class, continue, def, del, elif, else,
-except, exec, False, finally, for, from, global, if, import, in,
-is, lambda, not, None, or, pass, print, raise, return, try, True,
-while, with, yield
+first_string = 245
+second_number = "This isn't a number"
+print(first_string)
+print(second_number)
