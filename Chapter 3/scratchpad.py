086e4b11008fe1edab672bd70ffc3e70bf7e6535
Richard <rwhittaker86@outlook.com>
2018-03-02 10:34:24 -0500

    changing data in variables example in swcratch pad

--- Chapter 3/scratchpad.py
+++ Chapter 3/scratchpad.py
@@ -1,4 +1,7 @@
-first_string = 245
-second_number = "This isn't a number"
-print(first_string)
-print(second_number)
+proverb = "A penny saved"
+proverb = proverb + " is a penny earned"
+print(proverb)
+pennies_saved=0
+pennies_saved=pennies_saved +1
+print(pennies_saved)
+print(pennies_saved+1)
