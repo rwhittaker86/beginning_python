5b4c66377c0e2e5bf77c8ba708f0703ccf2b266a
Richard <rwhittaker86@outlook.com>
2018-07-11 22:10:57 -0400

    trying out creating and using a tuple in scratch pad

--- Chapter 3/scratchpad.py
+++ Chapter 3/scratchpad.py
@@ -1,7 +1,3 @@
-proverb = "A penny saved"
-proverb = proverb + " is a penny earned"
-print(proverb)
-pennies_saved=0
-pennies_saved=pennies_saved +1
-print(pennies_saved)
-print(pennies_saved+1)
+filler = ("string", "filled", "by a", "tuple")
+print("A %s %s %s %s" % ("stirng", "filled", "by a", "tuple"))
+print(filler)
