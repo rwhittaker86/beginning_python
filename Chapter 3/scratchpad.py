7c1769c4369cde43c738a871de38cbd1814a9461
Richard <rwhittaker86@outlook.com>
2018-07-26 01:12:12 -0400

    example of tuple removed added example of list and started Dictionary example

--- Chapter 3/scratchpad.py
+++ Chapter 3/scratchpad.py
@@ -1,7 +1,20 @@
-a = ("first", "second", "third")
-b = (a, "b's second element")
-print("%s" % b[1])
-print("%s" % b[0][0])
-print("%s" % b[0][1])
-print("%s" % b[0][2])
-single_element_tuple = ("the sole element",)
+breakfast= [ "coffee", "tea", "toast", "egg"]
+count = 0
+print("Today's breakfast is %s" % breakfast[count])
+count = 1
+print("Today's breakfast is %s" % breakfast[count])
+count = 2
+print("Today's breakfast is %s" % breakfast[count])
+count = 3
+print("Today's breakfast is %s" % breakfast[count])
+#When accessing more than one element of a list apparently it's "essential" to use a variable to hold the numbered position(?)
+print(breakfast[0])
+#hmm "essential" maybe I'm guessing the book just wants to let the reader know about using iterator variables since it's not actually necessary
+breakfast[count]= "sausage" # apparently adhoc adding an entry to the breakfast list causes an error but we can overwrite one of the existing values
+print("Today's breakfast is %s" % breakfast[count])
+#but you can use a python method to easily add a list element
+breakfast.append("waffles")
+count = 4
+print("Today's breakfast is %s" % breakfast[count])
+breakfast.extend(["juice", "decaf", "oatmeal"])
+print(breakfast)
