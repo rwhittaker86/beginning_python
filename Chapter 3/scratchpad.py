16691437e9af8ca9926f76757af5e2056d7f808b
Richard <rwhittaker86@outlook.com>
2018-07-30 01:11:50 -0400

    Poping list elements example added to chapter 3 scratch pad

--- Chapter 3/scratchpad.py
+++ Chapter 3/scratchpad.py
@@ -77,3 +77,16 @@ print(apartment)
 apartment = []
 apartment.extend(living_room)
 print(apartment)
+
+todays_temperatures = [23, 32, 33, 31]
+todays_temperatures.append(29)
+print(todays_temperatures)
+morning = todays_temperatures.pop(0)
+print("This mornings temperature was %.02f" % morning)
+late_morning = todays_temperatures.pop(0)
+print("Todays late morning temperature was %.02f"  % late_morning)
+noon = todays_temperatures.pop(0)
+print("Todays noon temperature as %.02f" % noon)
+print(todays_temperatures)
+print("Afternoon temperature was %.02f" % todays_temperatures.pop(0))
+print(todays_temperatures)
