3352bac5c24242a90e97ae5d5a2d1724caf2bde9
Richard <rwhittaker86@outlook.com>
2018-07-30 00:56:55 -0400

    Slicing examples added to chapter 3 scratch pad

--- Chapter 3/scratchpad.py
+++ Chapter 3/scratchpad.py
@@ -59,3 +59,13 @@ print("%s" % last_names[last_element])
 print("%s" % last_names[-1])
 print("%s" % last_names[-2])
 print("%s" % last_names[-3])
+
+slice_me = ("The", "next", "time", "we", "meet", "drinks", "are", "on", "me")
+sliced_tuple = slice_me[5:9]
+print(sliced_tuple)
+slice_this_list = ["The", "next", "time", "we", "meet", "drinks", "are", "on", "me"]
+sliced_list = slice_this_list[5:9]
+print(sliced_list)
+slice_this_string = "The next time we meet, drinks are on me"
+sliced_string = slice_this_string[5:9]
+print(sliced_string)
