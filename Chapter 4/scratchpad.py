c8dd0245dd6651a4273d8f83d2c5b63c98fef432
Richard <rwhittaker86@outlook.com>
2018-08-04 00:56:07 -0400

    finished ading comparing values for sameness example to chapter 4 scratchpad

--- Chapter 4/scratchpad.py
+++ Chapter 4/scratchpad.py
@@ -16,3 +16,9 @@ apple_trees = ["Golden Delicious", "Fuji", "Mitsu", "Mackintosh"]
 print(apples == apple_trees)
 apple_trees = ["Mackintosh", "Golden Delicious", "Fuji", "Mitsu"]
 print(apples == apple_trees)
+#example 2
+tuesday_breakfast_sold = {"pancakes":10, "french toast":4, "bagels":32, "omlets":12, "eggs and sausage":13}
+wednesday_breakfast_sold = {"pancakes":8, "french toast":5, "bagels":22, "omlets":16, "eggs and sausage":22}
+print(tuesday_breakfast_sold == wednesday_breakfast_sold)
+thursday_breakfast_sold = {"pancakes":10, "french toast":4, "bagels":32, "omlets": 12, "eggs and sausage":13}
+print(tuesday_breakfast_sold == thursday_breakfast_sold)
