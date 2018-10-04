c6236b56cdcd444feed6f5082f0fdb94731c9775
Richard <rwhittaker86@outlook.com>
2018-10-04 01:45:20 -0400

    added continue in loop example to chapter 4 scratchpad

--- Chapter 4/scratchpad.py
+++ Chapter 4/scratchpad.py
@@ -146,3 +146,8 @@ for food in ("pate", "cheese", "crackers"):
 else:
     print("There is no yogurt!")
 
+# conrinue example - simple
+for food in ("pate", "cheese", "rotten apples", "crackers", "whip cream", "tomato soup"):
+    if food[0:6] == "rotten":
+        continue
+    print("Hey you can eat %s" % food)
